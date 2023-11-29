3f3d6442c843276e779cfebb2d352220366d4dc6
Oluwadunsin david <oluwadunsindavid21@gmail.com>
2023-11-29 13:14:15 +0100

    Update

--- main.js
+++ main.js
@@ -14,3 +14,22 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+// PORTFOLIO
+
+const hamburger1 = document.querySelector(".hamburger1");
+const navMenu1 = document.querySelector(".nav-menu1");
+const navLink1 = document.querySelectorAll(".nav-link1");
+
+hamburger1.addEventListener("click", mobileMenu);
+navLink1.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger1.classList.toggle("active");
+    navMenu1.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger1.classList.remove("active");
+    navMenu1.classList.remove("active");
+}
